15b09b27ae8945a58e83d8df16f94a08ab1d26d8
rgupta00 <rgupta.mtech@gmail.com>
2023-09-20 17:31:32 +0530

    day 1

--- code/jersy/app2/src/com/demo/HelloResouce.java
+++ code/jersy/app2/src/com/demo/HelloResouce.java
@@ -1,8 +1,8 @@
 package com.demo;
 
-import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -10,21 +10,19 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class HelloResouce {
 	
-//	@GET
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public String hello() {
-//		return "hello";
-//	}
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "hello";
+	}
+
 	
-	//use json
-	//path Param
-	
-//	@GET
-//	@Path("/{uname}")
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public String hello2(@PathParam("uname")String name) {
-//		return "hello 2"+" name: "+name ;
-//	}
+	@GET
+	@Path("/{uname}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello2(@PathParam("uname")String name) {
+		return "hello 2"+" name: "+name ;
+	}
 	
 	//http://localhost:8090/app2/api/hello?uname=raj&city=noida
 	@GET
